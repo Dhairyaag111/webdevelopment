f4cf95d435b2d26b7ceefabf78bf66e7263fea6d
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-05-29 18:48:39 +0545

    new js

---
+++ Frontend/javascript/datatype.js
@@ -0,0 +1,5 @@
+// There are two type of datatype in java script.
+// 1.Primitive datatype.-
+// String, Number, Boolean, Null, Undefined, BigInt, etc. are some common datatypes in java script.
+// 2.Non-primitive datatype.-
+// Object, Array, Function are three non-primitive datatpye in java script.
--- Frontend/javascript/variable.js
+++ Frontend/javascript/variable.js
@@ -1,1 +1,5 @@
 // variable are the container that is used to store any kind of values. In java script we use three different ways to define a variable. Let, Var and Const 
+let name="Dhairya"
+var a=20
+const Pi=3.14
+prompt("enter any numbe")
