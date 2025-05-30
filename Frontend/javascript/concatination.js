408f5f287591f586073204184c6e0611e0f6a3d6
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-05-30 18:57:12 +0545

    area of square,rectangle,circle

--- Frontend/javascript/concatination.js
+++ Frontend/javascript/concatination.js
@@ -3,3 +3,15 @@ let t=10
 let r=10
 let si=(p*t*r/100)
 console.log("the simple interest is "+si)
+
+
+let l=10
+let b=5
+let radius=12
+const pi=3.141
+let area_of_square= l*l
+let area_of_rectangle= l*b
+let area_of_circle= pi*radius*radius
+console.log("area of square is "+area_of_square)
+console.log("area of rectangle is "+area_of_rectangle)
+console.log("area of circle is "+area_of_circle)
