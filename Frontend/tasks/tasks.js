89adbabbe758efd541ecd4c5dde47e1d1dab9112
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-05-31 11:41:34 +0545

    new file added

---
+++ Frontend/tasks/tasks.js
@@ -0,0 +1,10 @@
+let p=100000
+let t=2
+let r=12
+let compound_interest= p*(1+r/100)^t
+console.log("compound interest is "+compound_interest)
+
+let fx=1200
+let N=10
+let mean=fx/N
+console.log("the mean is "+mean)
