7fca22d92428c30076bfaf2bd0c7a92f37a7173a
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-05-31 12:07:11 +0545

    new

--- Frontend/tasks/tasks.js
+++ Frontend/tasks/tasks.js
@@ -14,10 +14,20 @@ let n=10
 let median=(n+1)/2 
 console.log("the median is "+median)
 
-let a=1
+let f=1
 let b=2
 let c=3
 let d=4
 let e=5
 let avg=(a+b+c+d+e)/5
 console.log("the avg is "+avg)
+
+let a=10
+let l=12
+let h=14
+let lsa=2*a*l
+let tsa=2*a*l+a*a
+let volume=(1/3)*a*a*h
+console.log("lsa is "+lsa)
+console.log("tsa is "+tsa)
+console.log("volume is "+volume)
