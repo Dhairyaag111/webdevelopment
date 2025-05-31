375177fbd6075e397f3f623e899cc64bc27e5d8c
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-05-31 12:00:58 +0545

    new

--- Frontend/tasks/tasks.js
+++ Frontend/tasks/tasks.js
@@ -1,7 +1,7 @@
 let p=100000
 let t=2
 let r=12
-let compound_interest= p*(1+r/100)^t
+let compound_interest=p*(1+(r/100))^t-1
 console.log("compound interest is "+compound_interest)
 
 let fx=1200
@@ -13,3 +13,11 @@ console.log("the mean is "+mean)
 let n=10
 let median=(n+1)/2 
 console.log("the median is "+median)
+
+let a=1
+let b=2
+let c=3
+let d=4
+let e=5
+let avg=(a+b+c+d+e)/5
+console.log("the avg is "+avg)
