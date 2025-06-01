d6adf1bb148a5e1ab8c85189be4f5137028eb64e
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-01 20:13:19 +0545

    all the tasks are up to date

--- Frontend/tasks/tasks2.js
+++ Frontend/tasks/tasks2.js
@@ -6,3 +6,15 @@ a=b
 b=c
 console.log("the value of 'a'is "+a)
 console.log("the value of 'b'is "+b)
+
+let d=20
+let e=40
+let f=60
+let g=null
+g=d
+d=e
+e=f
+f=g
+console.log("the value of 'd'is "+d)
+console.log("the value of 'e'is "+e)
+console.log("the value of 'f'is "+f)
