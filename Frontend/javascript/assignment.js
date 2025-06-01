95683e4157d436038447159e09293158b47313e2
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-01 18:35:16 +0545

    new file

---
+++ Frontend/javascript/assignment.js
@@ -0,0 +1,12 @@
+
+// Assignments operators in java script are used to  assign values to any variable and combine assignments with other operations.
+// Some assignment operator of java script are : =, +=, -=, *=, /=, %=
+
+let sum1=15
+sum1+=20
+console.log(sum1)
+
+
+let difference1=15
+difference1-=20
+console.log(difference1)
