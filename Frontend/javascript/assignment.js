83a956925ca37229c35b45d5cb42a38696f410c9
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-01 20:04:50 +0545

    new

--- Frontend/javascript/assignment.js
+++ Frontend/javascript/assignment.js
@@ -25,3 +25,16 @@ console.log(division1)
 let module1=150
 module1%=30
 console.log(module1)
+ 
+
+let product=5
+console.log("5x1 "+product*1)
+console.log("5x2 "+product*2)
+console.log("5x3 "+product*3)
+console.log("5x4 "+product*4)
+console.log("5x5 "+product*5)
+console.log("5x6 "+product*6)
+console.log("5x7 "+product*7)
+console.log("5x8 "+product*8)
+console.log("5x9 "+product*9)
+console.log("5x9 "+product*10)
---
+++ Frontend/tasks/tasks2.js
@@ -0,0 +1,8 @@
+let a=10
+let b=20
+let c=null
+c=a
+a=b
+b=c
+console.log("the value of 'a'is "+a)
+console.log("the value of 'b'is "+b)
