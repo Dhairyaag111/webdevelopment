ed2ffe8bce0c1ed207729ba26f4a0512564ad6ae
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-05 07:57:06 +0545

    new

--- Frontend/javascript/assignment.js
+++ Frontend/javascript/assignment.js
@@ -1,208 +1,228 @@
 
-// // // // // Assignments operators in java script are used to  assign values to any variable and combine assignments with other operations.
-// // // // // Some assignment operator of java script are : =, +=, -=, *=, /=, %=
+// // // // // // Assignments operators in java script are used to  assign values to any variable and combine assignments with other operations.
+// // // // // // Some assignment operator of java script are : =, +=, -=, *=, /=, %=
 
 
-// // // // let sum1=15
-// // // // sum1+=20
-// // // // console.log(sum1)
+// // // // // let sum1=15
+// // // // // sum1+=20
+// // // // // console.log(sum1)
 
 
-// // // // let difference1=15
-// // // // difference1-=20
-// // // // console.log(difference1)
+// // // // // let difference1=15
+// // // // // difference1-=20
+// // // // // console.log(difference1)
 
 
-// // // // let product1=15
-// // // // product1*=20
-// // // // console.log(product1)
+// // // // // let product1=15
+// // // // // product1*=20
+// // // // // console.log(product1)
 
 
-// // // // let division1=150
-// // // // division1/=30
-// // // // console.log(division1)
+// // // // // let division1=150
+// // // // // division1/=30
+// // // // // console.log(division1)
 
 
-// // // // let module1=150
-// // // // module1%=30
-// // // // console.log(module1)
+// // // // // let module1=150
+// // // // // module1%=30
+// // // // // console.log(module1)
  
 
-// // // // // let product=5
-// // // // // console.log("5x1 "+product*1)
-// // // // // console.log("5x2 "+product*2)
-// // // // // console.log("5x3 "+product*3)
-// // // // // console.log("5x4 "+product*4)
-// // // // // console.log("5x5 "+product*5)
-// // // // // console.log("5x6 "+product*6)
-// // // // // console.log("5x7 "+product*7)
-// // // // // console.log("5x8 "+product*8)
-// // // // // console.log("5x9 "+product*9)
-// // // // // console.log("5x9 "+product*10)
+// // // // // // let product=5
+// // // // // // console.log("5x1 "+product*1)
+// // // // // // console.log("5x2 "+product*2)
+// // // // // // console.log("5x3 "+product*3)
+// // // // // // console.log("5x4 "+product*4)
+// // // // // // console.log("5x5 "+product*5)
+// // // // // // console.log("5x6 "+product*6)
+// // // // // // console.log("5x7 "+product*7)
+// // // // // // console.log("5x8 "+product*8)
+// // // // // // console.log("5x9 "+product*9)
+// // // // // // console.log("5x9 "+product*10)
 
 
-// // // // let product = prompt("enter a number to show its table:")
-// // // // console.log(product+"x1="+product*1)
-// // // // console.log(product+"x2="+product*2)
-// // // // console.log(product+"x3="+product*3)
-// // // // console.log(product+"x4="+product*4)
-// // // // console.log(product+"x5="+product*5)
-// // // // console.log(product+"x6="+product*6)
-// // // // console.log(product+"x7="+product*7)
-// // // // console.log(product+"x8="+product*8)
-// // // // console.log(product+"x9="+product*9)
-// // // // console.log(product+"x10="+product*10)
+// // // // // let product = prompt("enter a number to show its table:")
+// // // // // console.log(product+"x1="+product*1)
+// // // // // console.log(product+"x2="+product*2)
+// // // // // console.log(product+"x3="+product*3)
+// // // // // console.log(product+"x4="+product*4)
+// // // // // console.log(product+"x5="+product*5)
+// // // // // console.log(product+"x6="+product*6)
+// // // // // console.log(product+"x7="+product*7)
+// // // // // console.log(product+"x8="+product*8)
+// // // // // console.log(product+"x9="+product*9)
+// // // // // console.log(product+"x10="+product*10)
 
  
-// // // // let num = Number(prompt("Enter any number : "))
-// // // // if (num>0){
-// // // //     console.log("The number is positive!")
+// // // // // let num = Number(prompt("Enter any number : "))
+// // // // // if (num>0){
+// // // // //     console.log("The number is positive!")
+// // // // // }
+
+ 
+// // // // // let numb = Number(prompt("Enter any number : "))
+// // // // // if (numb<0){
+// // // // //     console.log("The number is negative!")
+// // // // // } 
+
+ 
+// // // // // let numbe = Number(prompt("Enter any number : "))
+// // // // // if (numbe==0){
+// // // // //     console.log("The number is zero!")
+// // // // // }
+
+
+// // // // // let number = Number(prompt("enter a number:"))
+// // // // // if(number%2==0){
+// // // // //     console.log("the number is even")
+// // // // // }
+// // // // // else{
+// // // // //     console.log("the number is odd")
+// // // // // }
+
+
+// // // // // let marks = Number(prompt("enter your marks:"))
+// // // // // if(marks>=80){
+// // // // //     console.log("you got distinction")
+// // // // // }
+// // // // // else if(marks>=70){
+// // // // // console.log("you got first division")
+// // // // // }
+// // // // // else if(marks>=60){
+// // // // // console.log("you got second division")
+// // // // // }
+// // // // // else if(marks>=50){
+// // // // // console.log("you got third division")
+// // // // // }
+// // // // // else{
+// // // // //     console.log("you are failed")
+// // // // // }
+
+
+// // // // // // let num = Number(prompt("enter a number :"))
+// // // // // // if(num%10==0){
+// // // // // //     console.log("the number is divisible by 10")
+// // // // // // }
+// // // // // // else{
+// // // // // //     console.log("the number is not divisible by 10")
+// // // // // // }
+
+
+// // // // // // let num = Number(prompt("enter a number :"))
+// // // // // // if(num%7==0 && num%13==0){
+// // // // // //     console.log("the number is divisible by 7 and 13")
+// // // // // // }
+// // // // // // else{
+// // // // // //     console.log("the number is not divisible by 7 and 13")
+// // // // // // }
+
+
+// // // // let num = Number(prompt("enter a number between 1 and 7:"))
+// // // // if(num==1){
+// // // //     console.log("today is sunday")
 // // // // }
-
- 
-// // // // let numb = Number(prompt("Enter any number : "))
-// // // // if (numb<0){
-// // // //     console.log("The number is negative!")
-// // // // } 
-
- 
-// // // // let numbe = Number(prompt("Enter any number : "))
-// // // // if (numbe==0){
-// // // //     console.log("The number is zero!")
+// // // // else if(num==2){
+// // // //     console.log("today is monday")
 // // // // }
-
-
-// // // // let number = Number(prompt("enter a number:"))
-// // // // if(number%2==0){
-// // // //     console.log("the number is even")
+// // // // else if(num==3){
+// // // //     console.log("today is tuesday")
+// // // // }
+// // // // else if(num==4){
+// // // //     console.log("today is wednesday")
+// // // // }
+// // // // else if(num==5){
+// // // //     console.log("today is thursday")
+// // // // }
+// // // // else if(num==6){
+// // // //     console.log("today is friday")
+// // // // }
+// // // // else if(num==7){
+// // // //     console.log("today is saturday")
 // // // // }
 // // // // else{
-// // // //     console.log("the number is odd")
+// // // //     console.log("sorry!there is no day")
 // // // // }
 
 
-// // // // let marks = Number(prompt("enter your marks:"))
-// // // // if(marks>=80){
-// // // //     console.log("you got distinction")
+// // // // let marks = prompt("enter your marks:")
+// // // // if(marks>=90){
+// // // // console.log("you got A+")
+// // // // }
+// // // // else if(marks>=80){
+// // // //     console.log("you got A")
 // // // // }
 // // // // else if(marks>=70){
-// // // // console.log("you got first division")
+// // // //     console.log("you got B+")
 // // // // }
 // // // // else if(marks>=60){
-// // // // console.log("you got second division")
+// // // //     console.log("you got B")
 // // // // }
 // // // // else if(marks>=50){
-// // // // console.log("you got third division")
+// // // //     console.log("you got C+")
+// // // // }
+// // // // else if(marks>=40){
+// // // //     console.log("you got A")
 // // // // }
 // // // // else{
-// // // //     console.log("you are failed")
+// // // //     console.log("you are fail")
 // // // // }
-
-
-// // // // // let num = Number(prompt("enter a number :"))
-// // // // // if(num%10==0){
-// // // // //     console.log("the number is divisible by 10")
-// // // // // }
-// // // // // else{
-// // // // //     console.log("the number is not divisible by 10")
-// // // // // }
-
-
-// // // // // let num = Number(prompt("enter a number :"))
-// // // // // if(num%7==0 && num%13==0){
-// // // // //     console.log("the number is divisible by 7 and 13")
-// // // // // }
-// // // // // else{
-// // // // //     console.log("the number is not divisible by 7 and 13")
-// // // // // }
-
-
-// // // let num = Number(prompt("enter a number between 1 and 7:"))
-// // // if(num==1){
-// // //     console.log("today is sunday")
-// // // }
-// // // else if(num==2){
-// // //     console.log("today is monday")
-// // // }
-// // // else if(num==3){
-// // //     console.log("today is tuesday")
-// // // }
-// // // else if(num==4){
-// // //     console.log("today is wednesday")
-// // // }
-// // // else if(num==5){
-// // //     console.log("today is thursday")
-// // // }
-// // // else if(num==6){
-// // //     console.log("today is friday")
-// // // }
-// // // else if(num==7){
-// // //     console.log("today is saturday")
-// // // }
-// // // else{
-// // //     console.log("sorry!there is no day")
-// // // }
-
-
-// // // let marks = prompt("enter your marks:")
-// // // if(marks>=90){
-// // // console.log("you got A+")
-// // // }
-// // // else if(marks>=80){
-// // //     console.log("you got A")
-// // // }
-// // // else if(marks>=70){
-// // //     console.log("you got B+")
-// // // }
-// // // else if(marks>=60){
-// // //     console.log("you got B")
-// // // }
-// // // else if(marks>=50){
-// // //     console.log("you got C+")
-// // // }
-// // // else if(marks>=40){
-// // //     console.log("you got A")
-// // // }
-// // // else{
-// // //     console.log("you are fail")
-// // // }
  
 
-// // let num= Number(prompt("enter a number:"))
-// // if(num>0){
-// //     if(num%2==0){
-// //         console.log("the number is even as well as positive")
-// //     }
-// //     else{
-// //         console.log("the number is odd as well as positive")
-// //     }
+// // // let num= Number(prompt("enter a number:"))
+// // // if(num>0){
+// // //     if(num%2==0){
+// // //         console.log("the number is even as well as positive")
+// // //     }
+// // //     else{
+// // //         console.log("the number is odd as well as positive")
+// // //     }
+// // // }
+// // // else if(num<0){
+// // //        if(num%2==0){
+// // //         console.log("the number is even as well as negative")
+// // //     }
+// // //     else{
+// // //         console.log("the number is odd as well as negative")
+// // //     }
+// // // }
+// // // else{
+// // //     console.log("the number is equal to zero and it is also even")
+// // // }
+
+
+// // // let amt= Number(prompt("enter your amount:"))
+// // if(amt<=10000){
+// //     dis = amt*(2/100)
+// //     console.log("your discount amount is "+dis)
 // // }
-// // else if(num<0){
-// //        if(num%2==0){
-// //         console.log("the number is even as well as negative")
-// //     }
-// //     else{
-// //         console.log("the number is odd as well as negative")
-// //     }
+// // else if(amt<=50000){
+// //     dis = amt*(10/100)
+// //     console.log("your discount amount is "+dis)
+// // }
+// // else if(amt<=100000){
+// //     dis = amt*(15/100)
+// //     console.log("your discount amount is  "+dis)
 // // }
 // // else{
-// //     console.log("the number is equal to zero and it is also even")
+// //     console.log("sorry!no discount for you.")
 // // }
 
 
-// // let amt= Number(prompt("enter your amount:"))
-// if(amt<=10000){
-//     dis = amt*(2/100)
-//     console.log("your discount amount is "+dis)
+// let a = Number(prompt("enter your purchasing amount:"))
+// if (a<=10000){
+//     dis=a*(2/100)
+//     console.log("You got 2% discount" )
+//     console.log("Your total discount amt is : Rs"+dis)
 // }
-// else if(amt<=50000){
-//     dis = amt*(10/100)
-//     console.log("your discount amount is "+dis)
+// else if(a<=50000){
+//     let discount=a-10000
+//     dis=(discount*(10/100))+(10000*(2/100))
+//     console.log("You got 2% discount on first Rs10000 and 10% discount on remaining.")
+//     console.log("Your total discount amt is : Rs"+dis)
 // }
-// else if(amt<=100000){
-//     dis = amt*(15/100)
-//     console.log("your discount amount is  "+dis)
-// }
-// else{
-//     console.log("sorry!no discount for you.")
+// else if(a>=100000){
+//     let discount=a-50000
+//     dis=(discount*(15/100))+(40000*(10/100))+(10000*(2/100))
+//     console.log("You got 2% discount on first Rs10000 and 10% discount on Rs50000 and 15% on remaining amount.")
+//   console.log("Your total discount amt is : Rs"+dis)
 // }
