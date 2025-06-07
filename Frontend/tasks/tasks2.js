f81d5b8acb27a8be716acae200915041a4cc8dae
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-07 12:18:06 +0545

    new

--- Frontend/tasks/tasks2.js
+++ Frontend/tasks/tasks2.js
@@ -1,25 +1,3 @@
-// // let a=10
-// // let b=20
-// // let c=null
-// // c=a
-// // a=b
-// // b=c
-// // console.log("the value of 'a'is "+a)
-// // console.log("the value of 'b'is "+b)
-
-
-// // let d=20
-// // let e=40
-// // let f=60
-// // let g=null
-// // g=d
-// // d=e
-// // e=f
-// // f=g
-// // console.log("the value of 'd'is "+d)
-// // console.log("the value of 'e'is "+e)
-// // console.log("the value of 'f'is "+f)
-
 
 // // let y= Number(prompt("Enter any year:"))
 // // if(y%4==0){
