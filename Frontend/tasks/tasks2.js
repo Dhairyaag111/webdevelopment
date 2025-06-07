85ad1f659ea18a60b6be06348e38025f5075e7de
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-07 12:11:29 +0545

    new

--- Frontend/tasks/tasks2.js
+++ Frontend/tasks/tasks2.js
@@ -1,30 +1,54 @@
-let a=10
-let b=20
-let c=null
-c=a
-a=b
-b=c
-console.log("the value of 'a'is "+a)
-console.log("the value of 'b'is "+b)
+// let a=10
+// let b=20
+// let c=null
+// c=a
+// a=b
+// b=c
+// console.log("the value of 'a'is "+a)
+// console.log("the value of 'b'is "+b)
 
 
-let d=20
-let e=40
-let f=60
-let g=null
-g=d
-d=e
-e=f
-f=g
-console.log("the value of 'd'is "+d)
-console.log("the value of 'e'is "+e)
-console.log("the value of 'f'is "+f)
+// let d=20
+// let e=40
+// let f=60
+// let g=null
+// g=d
+// d=e
+// e=f
+// f=g
+// console.log("the value of 'd'is "+d)
+// console.log("the value of 'e'is "+e)
+// console.log("the value of 'f'is "+f)
 
 
-let y= Number(prompt("Enter any year:"))
-if(y%4==0){
-    console.log("The year you entered is a leap year.")
-}
-else{
-        console.log("The year you entered is a normal year.")
-}
+// let y= Number(prompt("Enter any year:"))
+// if(y%4==0){
+//     console.log("The year you entered is a leap year.")
+// }
+// else{
+//         console.log("The year you entered is a normal year.")
+// }
+
+
+// let a=Number(prompt("Enter a number:"))
+// let b=Number(prompt("Enter another number:"))
+// let c=prompt("Enter an operator [add(+),sub(-),mul(*),div(/)]:")
+// if (c=='+') {
+//     sum = a+b
+//     console.log("The sum is "+sum)
+// }
+// else if (c=='-'){
+//     diff = a-b
+//     console.log("The difference is "+diff)
+// }
+// else if (c=='*'){
+//     pr = a*b
+//     console.log("The product is "+pr)
+// }
+// else if (c=='/'){
+//     div = a/b
+//     console.log("The division is "+div)
+// }
+// else{
+//     console.log("The operator you entered is incorrect.")
+// }
