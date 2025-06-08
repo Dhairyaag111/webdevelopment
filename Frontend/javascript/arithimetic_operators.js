18a1ef66161696a687dd477b767956b4754726ee
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-08 18:42:29 +0545

    new

--- Frontend/javascript/arithimetic_operators.js
+++ Frontend/javascript/arithimetic_operators.js
@@ -1,25 +1,25 @@
-// let firstNum=1
-// let secondNum=2
+// // let firstNum=1
+// // let secondNum=2
+// // let sum=firstNum+secondNum
+// // let diff=firstNum-secondNum
+// // console.log(sum)
+// // console.log(diff)
+
+
+// let firstNum=11
+// let secondNum=22
 // let sum=firstNum+secondNum
-// let diff=firstNum-secondNum
+// let difference=firstNum-secondNum
+// let product=firstNum*secondNum
+// let division=firstNum/secondNum
 // console.log(sum)
-// console.log(diff)
+// console.log(difference)
+// console.log(product)
+// console.log(division)
 
-
-let firstNum=11
-let secondNum=22
-let sum=firstNum+secondNum
-let difference=firstNum-secondNum
-let product=firstNum*secondNum
-let division=firstNum/secondNum
-console.log(sum)
-console.log(difference)
-console.log(product)
-console.log(division)
-
-// simple interest program
-let p=100000
-let t=10
-let r=10
-let si=(p*t*r/100)
-console.log(si)
+// // simple interest program
+// let p=100000
+// let t=10
+// let r=10
+// let si=(p*t*r/100)
+// console.log(si)
--- Frontend/tasks/tasks2.js
+++ Frontend/tasks/tasks2.js
@@ -1,56 +1,77 @@
 
-// // // let y= Number(prompt("Enter any year:"))
-// // // if(y%4==0){
-// // //     console.log("The year you entered is a leap year.")
-// // // }
-// // // else{
-// // //         console.log("The year you entered is a normal year.")
-// // // }
+// // // // let y= Number(prompt("Enter any year:"))
+// // // // if(y%4==0){
+// // // // console.log("The year you entered is a leap year.")
+// // // // }
+// // // // else{
+// // // // console.log("The year you entered is a normal year.")
+// // // // }
 
 
-// // // let a=Number(prompt("Enter a number:"))
-// // // let b=Number(prompt("Enter another number:"))
-// // // let c=prompt("Enter an operator [add(+),sub(-),mul(*),div(/)]:")
-// // // if (c=='+') {
-// // //     sum = a+b
-// // //     console.log("The sum is "+sum)
-// // // }
-// // // else if (c=='-'){
-// // //     diff = a-b
-// // //     console.log("The difference is "+diff)
-// // // }
-// // // else if (c=='*'){
-// // //     pr = a*b
-// // //     console.log("The product is "+pr)
-// // // }
-// // // else if (c=='/'){
-// // //     div = a/b
-// // //     console.log("The division is "+div)
-// // // }
-// // // else{
-// // //     console.log("The operator you entered is incorrect.")
-// // // }
-
-
-// // let t = Number(prompt("Enter the temperture of any place:"))
-// // if(t<15){
-// //     console.log("The place is very cold.")
+// // let a=Number(prompt("Enter a number:"))
+// // let b=Number(prompt("Enter another number:"))
+// // let c=prompt("Enter an operator [add(+),sub(-),mul(*),div(/)]:")
+// // if (c=='+') {
+// //     sum = a+b
+// //     console.log("The sum is "+sum)
 // // }
-// // else if(t>=15 && t<30){
-// //     console.log("The place is normal.")
+// // else if (c=='-'){
+// //     diff = a-b
+// //     console.log("The difference is "+diff)
+// // }
+// // else if (c=='*'){
+// //     pr = a*b
+// //     console.log("The product is "+pr)
+// // }
+// // else if (c=='/'){
+// //     div = a/b
+// //     console.log("The division is "+div.toFixed(2))
 // // }
 // // else{
-// //         console.log("The place is very hot.")
+// //     console.log("The operator you entered is incorrect.")
 // // }
 
 
-// let d= Number(prompt("Enter the first angle of triangle:"))
-// let e= Number(prompt("Enter the second angle of triangle:"))
-// let g= Number(prompt("Enter the third angle of triangle:"))
-// sum = d+e+g
-// if(sum==180){
-//     console.log("This is a perfect triangle")
+// // // let t = Number(prompt("Enter the temperture of any place:"))
+// // // if(t<15){
+// // //     console.log("The place is very cold.")
+// // // }
+// // // else if(t>=15 && t<30){
+// // //     console.log("The place is normal.")
+// // // }
+// // // else{
+// // //         console.log("The place is very hot.")
+// // // }
+
+
+// // let d= Number(prompt("Enter the first angle of triangle:"))
+// // let e= Number(prompt("Enter the second angle of triangle:"))
+// // let g= Number(prompt("Enter the third angle of triangle:"))
+// // sum = d+e+g
+// // if(sum==180){
+// //     console.log("This is a perfect triangle")
+// // }
+// // else{
+// //    console.log("This is  not a triangle")    
+// // }
+
+
+// let u=prompt("Enter rock, paper, scissor:")
+// let c='rock'
+// if(u==c)
+// {
+//     console.log("Sorry!You Lost.")
 // }
-// else{
-//    console.log("This is  not a triangle")    
+// else if(u=="paper" && c=="rock" )
+// {
+//         console.log("Cngratulations!You Won.")
 // }
+// else if(u=="scissor" &&  c=="rock" )
+// {
+//          console.log("Its Draw.")
+// }
+// else
+// {
+//     console.log("Your input is incorrect.")
+// }
+
