530599182ffbc707b78e197cf4a18ec8ece7338a
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-10 20:51:34 +0545

    all file comented

--- Frontend/tasks/tasks.js
+++ Frontend/tasks/tasks.js
@@ -1,9 +1,9 @@
-for(i=1;i<=5;i=i+1){
-    console.log(i*i*i)
-}
+// for(i=1;i<=5;i=i+1){
+//     console.log(i*i*i)
+// }
 
 
-for(i=1;i<=10000;i=i*10){
-    console.log(i)
-}
+// for(i=1;i<=10000;i=i*10){
+//     console.log(i)
+// }
 
