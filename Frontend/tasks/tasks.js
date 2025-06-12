8d9ece2bafa9a78346c2a97aad468f9a019f985a
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-12 14:06:00 +0545

    new

--- Frontend/tasks/tasks.js
+++ Frontend/tasks/tasks.js
@@ -16,3 +16,32 @@
 // for(i=3;i<=33333;i=i*10+3){
 //     console.log(i)
 // }
+
+
+// let c =prompt("Enter the name of country to khow its currency:")
+// if(c=='Afganistan' || c=='afganistan'){
+//     console.log("The currrency of Afganistan is Afgani(AFN)")
+// }
+// else if(c=='Albania' || c=='albania'){
+//     console.log("The currrency of Albania is Albanian Lek(ALL)")
+// }
+// else if(c=='Algeria' || c=='algeria'){
+//     console.log("The currrency of Algeria is Algerian Dinar(DZD) ")
+// }
+// else if(c=='Andorra' || c=='andorra'){
+//     console.log("The currrency of Andorra is Euro(EUR) ")
+// }
+// else if(c=='Angola' || c=='angola'){
+//     console.log("The currrency of Angola is Angolan Kwanza(AOA) ")
+// }
+// else if(c=='Antigua and Barbuda' || c=='antigua and barbuda'){
+//     console.log("The currrency of 	Antigua and Barbuda is East Caribbean Dollar(EC$)}
+// else if(c=='Argentina' || c=='argentina'){
+//     console.log("The currrency of Argentina is Argentine Peso (ARS)")
+// }
+// else if(c=='Armenia' || c=='armenia'){
+//     console.log("The currrency of Armenia is Armenian Dram (AMD)")
+// }
+// else if(c=='Armenia' || c=='armenia'){
+//     console.log("The currrency of Armenia is Armenian Dram (AMD)")
+// }
