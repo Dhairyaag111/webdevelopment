cd71146edc9440936dcd1062c995a6b21346e1c8
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-12 14:20:22 +0545

    new

--- Frontend/tasks/tasks.js
+++ Frontend/tasks/tasks.js
@@ -18,7 +18,7 @@
 // }
 
 
-// let c =prompt("Enter the name of country to khow its currency:")
+// let c =prompt("Enter the name of country to khow its currency: ")
 // if(c=='Afganistan' || c=='afganistan'){
 //     console.log("The currrency of Afganistan is Afgani(AFN)")
 // }
@@ -35,13 +35,40 @@
 //     console.log("The currrency of Angola is Angolan Kwanza(AOA) ")
 // }
 // else if(c=='Antigua and Barbuda' || c=='antigua and barbuda'){
-//     console.log("The currrency of 	Antigua and Barbuda is East Caribbean Dollar(EC$)}
+//     console.log("The currrency of 	Antigua and Barbuda is East Caribbean Dollar(ECD")}
 // else if(c=='Argentina' || c=='argentina'){
 //     console.log("The currrency of Argentina is Argentine Peso (ARS)")
 // }
 // else if(c=='Armenia' || c=='armenia'){
 //     console.log("The currrency of Armenia is Armenian Dram (AMD)")
 // }
-// else if(c=='Armenia' || c=='armenia'){
-//     console.log("The currrency of Armenia is Armenian Dram (AMD)")
+// else if(c=='Australia' || c=='australia'){
+//     console.log("The currrency of Australia is Australian Dollar (AUD)")
+// }
+// else if(c=='Austria' || c=='austria'){
+//     console.log("The currrency of Austria is Euro (EUR)")
+// }
+// else if(c=='Azerbaijan' || c=='azerbaijan'){
+// console.log("The currrency of Azerbaijan is Azerbaijani Manat  (AZN)")
+// }
+// else if(c=='Bahamas' || c=='bahamas'){
+// console.log("The currrency of Bahamas is Bahamian Dollar (BSD)")
+// }
+// else if(c=='Bahrain' || c=='bahrain'){
+// console.log("The currrency of Bahrain is Bahraini Dinar (BHD)")
+// }
+// else if(c=='Bangladesh' || c=='bangladesh'){
+// console.log("The currrency of Bangladesh is Bangladeshi Taka (TK)")
+// }
+// else if(c=='Bangladesh' || c=='bangladesh'){
+// console.log("The currrency of Bangladesh is Bangladeshi Taka (TK)")
+// }
+// else if(c=='Barbados' || c=='barbados'){
+// console.log("The currrency of Barbados is  Barbados Dollar (BBD)")
+// }	
+// else if(c=='Belarus' || c=='belarus'){
+// console.log("The currrency of Belarus is Belarusian Ruble (BYN)")
+// }	
+// else if(c=='Belarus' || c=='belarus'){
+// console.log("The currrency of Belarus is Belarusian Ruble (BYN)")
 // }
