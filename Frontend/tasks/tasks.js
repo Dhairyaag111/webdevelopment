c2d9b0a20352b5f9b23d4f3e893af853dea49a56
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-13 19:08:19 +0545

    new

--- Frontend/tasks/tasks.js
+++ Frontend/tasks/tasks.js
@@ -18,7 +18,7 @@
 // }
 
 
-// let c =prompt("Enter the name of country to khow its currency: ")
+// let c=prompt("Enter the name of country to khow its currency: ")
 // if(c=='Afganistan' || c=='afganistan'){
 //     console.log("The currrency of Afganistan is Afgani(AFN)")
 // }
@@ -106,3 +106,244 @@
 // else if(c=='Burundi' || c=='burundi'){
 // console.log("The currrency of Burundi is Burundian Franc (BIF)")
 // }
+// else if(c=='Cambodia' || c=='cambodia'){
+// console.log("The currrency of Cambodia is Cambodian Riel (KHR))")
+// }
+// else if(c=='China' || c=='china'){
+// console.log("The currrency of China is Chinese Yuan Renminbi (CYN))")
+// }
+// else if(c=='Cyprus' || c=='Cyprus'){
+// console.log("The currrency of Cyprus is Cypriot Pound (CYP) ")
+// }
+// else if(c=='Georgia' || c=='Georgia'){
+// console.log("The currrency of Georgia is Georgian Lari (GELIndia)")
+// }
+// else if(c=='India' || c=='India'){
+// console.log("The currrency of India is Indian Rupee (INRIndonesia)")
+// }
+// else if(c=='Indonesia' || c=='Indonesia'){
+// console.log("The currrency of Indonesia is Indonesian Rupiah (IDR)")
+// }
+// else if(c=='Iran' || c=='Iran'){
+// console.log("The currrency of Iran is Iranian Rial (TRR)")
+// }
+// else if(c=='Iraq' || c==Iraq'){
+// console.log("The currrency of Iraq is Iraqi Dinar (IQDIsrael)")
+// }
+// else if(c=='Israel' || c=='Israel'){
+// console.log("The currrency of Israel is Israeli New Sheqel (ILS)")
+// }
+// else if(c=='Japan' || c=='Japan'){
+// console.log("The currrency of Japan is Japanese Yen (JPYJordan)")
+// }
+// else if(c=='Jordan' || c=='Jordan'){
+// console.log("The currrency of Jordan is Jordanian Dinar(JODKazakhstan)")
+// }
+// else if(c=='Kazakhstan' || c=='Kazakhstan'){
+// console.log("The currrency of Kazakhstan is (KZT)")
+// }
+// else if(c=='Kuwait'){
+// console.log("The currrency of Kuwait is Kuwaiti Dinar (KWD)")
+// }
+// else if(c=='Kyrgyzstan'){
+// console.log("The currrency of Kyrgyzstan is Kyrgyzstani Som (KGS)")
+// }
+// else if(c=='Laos'){
+// console.log("The currrency of Laos is Lao Kip (LAK)")
+// }
+// else if(c=='Lebanon'){
+// console.log("The currrency of Lebanon is Lebanese Pound (LBP)")
+// }
+// else if(c=='Malaysia'){
+// console.log("The currrency of Malaysia is Malaysian Ringgit (MYR)")
+// }
+// else if(c=='Maldives'){
+// console.log("The currrency of Maldives is Maldives Rufiyaa (MVR)")
+// }
+// else if(c=='Mongolia'){
+// console.log("The currrency of Mongolia is Mongolian Tugrik (MNT)")
+// }
+// else if(c=='Myanmar'){
+// console.log("The currrency of Myanmar is Myanmar Kyat (MMK)")
+// }
+// else if(c=='Nepal'){
+// console.log("The currrency of Nepal is Nepalese Rupee (NPR)")
+// }
+// else if(c=='North Korea'){
+// console.log("The currrency of North Korea is North Korean Won (KPW)")
+// }
+// else if(c=='Oman'){
+// console.log("The currrency of Oman is Omani Rial (OMR)")
+// }
+// else if(c=='Pakistan'){
+// console.log("The currrency of Pakistan is Pakistan Rupee (PKR)")
+// }
+// else if(c=='Palestine'){
+// console.log("The currrency of Palestine is Jordanian Dinar (JOD)")
+// }
+// else if(c=='Philippines'){
+// console.log("The currrency of Philippines is Philippine Peso (PHP)")
+// }
+// else if(c=='Qatar'){
+// console.log("The currrency of Qatar is Qatari Riyal (QAR)")
+// }
+// else if(c=='Russia'){
+// console.log("The currrency of Russia is Russian Ruble (RUB)")
+// }
+// else if(c=='Saudi Arabia'){
+// console.log("The currrency of Saudi Arabia is Saudi Arabian Riyal (SAR)")
+// }
+// else if(c=='Singapore'){
+// console.log("The currrency of Singapore is Singapore Dollar (SGD)")
+// }
+// else if(c=='South Korea'){
+// console.log("The currrency of South Korea is Korean Won (KRW)")
+// }
+// else if(c=='Sri Lanka'){
+// console.log("The currrency of Sri Lanka is Sri Lankan Rupee (LKR)")
+// }
+// else if(c=='Syria'){
+// console.log("The currrency of Syria is Syrian Pound (SYP)")
+// }
+// else if(c=='Taiwan'){
+// console.log("The currrency of Taiwan is New Taiwan Dollar (TWD)")
+// }
+// else if(c=='Tajikistan'){
+// console.log("The currrency of Tajikistan is Tajikistan Somoni (TJS)")
+// }
+// else if(c=='Thailand'){
+// console.log("The currrency of Thailand is Thai Baht (THB)")
+// }
+// else if(c=='Timor-Leste'){
+// console.log("The currrency of Timor-Leste is United States Dollar (USD)")
+// }
+// else if(c=='Turkey'){
+// console.log("The currrency of Turkey is Turkish New Lira (TRY)")
+// }
+// else if(c=='Turkmenistan'){
+// console.log("The currrency of Turkmenistan is Turkmenistani Manat (TMM)")
+// }
+// else if(c=='United Arab Emirates'){
+// console.log("The currrency of United Arab Emirates is United Arab Emirates Dirham (AED)")
+// }
+// else if(c=='Uzbekistan'){
+// console.log("The currrency of Uzbekistan is Uzbekistani Som (UZS)")
+// }
+// else if(c=='Vietnam'){
+// console.log("The currrency of Vietnam is Viet Nam Dong (VND)")
+// }
+// else if(c=='Yemen'){
+// console.log("The currrency of Yemen is Yemeni Rial (YER)")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+// else if(c==''){
+// console.log("The currrency of  is ")
+// }
+
