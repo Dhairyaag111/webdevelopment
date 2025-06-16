f1f6c4ae03dc0bd31b738e7f560648ebca339b08
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-16 18:07:19 +0545

    1,2,4,7,11,16,22

--- Frontend/javascript/whileLoop.js
+++ Frontend/javascript/whileLoop.js
@@ -39,3 +39,57 @@
 //      console.log(a+"X"+i+"="+a*i)
 //     i=i+1
 // }
+
+
+// let sum=0
+// let i=1
+// while(i<=100){
+//     sum=sum+i
+//     i=i+1
+// }
+// console.log(sum)
+
+
+// let i=50
+// while(i>=1){
+// if(i%4==0){
+//     console.log(i)
+// }
+//       i=i-1
+// }
+
+
+// let i = 1
+// let a=1
+// while(i<=5){
+// console.log(a)
+// a=a*10+1
+// i++
+// }
+
+
+// let i=2
+// let a=1
+// while(i<=6){
+// console.log(a)
+// a=a*10+i
+// i=i+1
+// }
+
+
+// let i = 1
+// let a=2
+// while (i<=6){
+//     console.log(a)
+//     a=a*2
+//     i++
+// }
+
+
+// let i=1
+// let a=1
+// while (i<=7){
+//     console.log(a)
+//     a=a+i
+//     i++
+// }
