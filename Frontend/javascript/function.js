3ed667e7a2c5603e643db8924a9d87e641b98a1f
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-18 18:27:50 +0545

    function

---
+++ Frontend/javascript/function.js
@@ -0,0 +1,9 @@
+// function mulitplication(a){
+//     for(i=1;i<=10;i++){
+//         pro = a*i
+//         console.log(a+" X "+i+" = "+pro)
+//     }
+// }
+
+// let a = Number(prompt("Enter any number to know its table: "))
+// mulitplication(a)
