c415bb5a48ea66d01a40cb35258b03d165f254f2
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-18 18:49:11 +0545

    return

--- Frontend/javascript/function.js
+++ Frontend/javascript/function.js
@@ -7,3 +7,20 @@
 
 // let a = Number(prompt("Enter any number to know its table: "))
 // mulitplication(a)
+
+
+// function square(a){
+//     sq = a*a
+//     console.log("The square of "+a+" is "+sq)
+// }
+
+// function cube(a){
+//     cub = a*a*a
+//     return cub
+// } 
+
+// let a = Number(prompt("Enter the number to know its square and cube: "))
+
+// square(a)
+// let result = cube(a)
+// console.log("The cube of "+a+" is "+cub)
