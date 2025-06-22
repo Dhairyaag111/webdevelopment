29dfd3d2c2bf81d691734bdd46fbab38f3a054b3
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-22 20:24:22 +0545

    program for calculator using anonymous function

--- Frontend/javascript/anonymous.js
+++ Frontend/javascript/anonymous.js
@@ -50,8 +50,46 @@
 
 
 
-// let func = function(a){
-//     return a*a
-// }
+    // let func = function(a){
+    //     return a*a
+    // }
 
-// console.log("The square of 5 is "+func(5))
+    // console.log("The square of 5 is "+func(5))
+
+
+
+
+//     let func_add = function(a, b){
+//     return a + b;
+// }
+// let func_sub = function(a, b){
+//     return a - b;
+// }
+// let func_mul = function(a, b){
+//     return a * b;
+// }
+// let func_div = function(a, b){
+//     return a/b
+// }
+// function nextfunction(a, b, c) {
+//     if (c=='+') {
+//         console.log(`The sum of ${a} and ${b} is ${func_add(a, b)}`)
+//     } 
+//     else if (c=='-') {
+//         console.log(`The difference between ${a} and ${b} is ${func_sub(a, b)}`)
+//     } 
+//     else if (c=='*') {
+//         console.log(`The product of ${a} and ${b} is ${func_mul(a, b)}`)
+//     } 
+//     else if (c=='/') {
+//         console.log(`The division of ${a} and ${b} is ${func_div(a, b)}`)
+//     }
+//     else {
+//         console.log("The operator you enter is incorrect")
+//     }
+// }
+// let a = Number(prompt("Enter the first number: "))
+// let b = Number(prompt("Enter the second number: "))
+// let c = prompt("Enter the operator (+, -, *, /): ")
+
+// nextfunction(a,b,c)
