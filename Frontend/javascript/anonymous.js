8d7cba1ddf7176a1c7b83dbd37eb2b6503aa2a86
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-22 18:26:38 +0545

    using parameter , ananymous

--- Frontend/javascript/anonymous.js
+++ Frontend/javascript/anonymous.js
@@ -1,1 +1,9 @@
- 
+//  let myFunc = function(a,b){
+//     return a*b
+// }
+
+// let a = Number(prompt("Enter first number: "))
+// let b = Number(prompt("Enter second number: "))
+
+// let res_myfunc = myFunc(a,b)
+// console.log("The product is "+res_myfunc)
