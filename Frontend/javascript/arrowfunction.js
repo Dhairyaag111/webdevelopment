46cf79e081c7390aad80d2306e0aeaf5fc7e04ac
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-23 21:55:35 +0545

    all the taske are completed

--- Frontend/javascript/arrowfunction.js
+++ Frontend/javascript/arrowfunction.js
@@ -5,6 +5,9 @@
 // func(5,2)
 
 
+
+
+
 // let func = =>{
 //     console.log(a+b)
 // }
@@ -22,6 +25,7 @@
 // const pi = 3.1415
 // let r = Number(prompt("Enter the radius of the circle(in cm): "))
 // console.log("The are of circle is "+func(r).toFixed(1)+" sq. cm")
+
 
 
 
