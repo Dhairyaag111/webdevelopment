01158ff2efbc258bd92b377e640cdcc7f527708b
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-23 20:38:44 +0545

    arrow function for area of circle

--- Frontend/javascript/arrowfunction.js
+++ Frontend/javascript/arrowfunction.js
@@ -3,3 +3,23 @@
 // }
 
 // func(5,2)
+
+
+// let func = =>{
+//     console.log(a+b)
+// }
+
+// func(5)
+
+
+
+
+
+//     let func = (r)=>{
+//     return pi*r*r
+// }
+
+// const pi = 3.1415
+// let r = Number(prompt("Enter the radius of the circle(in cm): "))
+// console.log("The are of circle is "+func(r).toFixed(1)+" sq. cm")
+
