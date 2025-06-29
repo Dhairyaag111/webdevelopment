a813520c4d6f43bb49344e000eceb052e0377bd2
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-29 18:38:43 +0545

    immediately invoked function expression jsva script to fine prime number

---
+++ Frontend/javascript/immediately_invoked_function_expression.js
@@ -0,0 +1,27 @@
+// let a = Number(prompt("Enter a number to check it is prime or not: "))
+
+
+// let func = (a)=>{
+//     let counter=0
+//     for(i=1;i<=a;i++){
+//         if(a%i==0){
+//             counter++
+//         }
+//     }
+//     if(counter==2){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// (function (a){
+//     if(a==true){
+//         console.log(`${a} is a prime number`)
+//     }
+//     else{
+//         console.log(`${a} is not a prime number`)
+//     }
+// }
+// )(func(a))
