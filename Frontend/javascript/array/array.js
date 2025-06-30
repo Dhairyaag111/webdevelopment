f252b2a6ab8f946e7802dfd56587bb52647c5aa7
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-06-30 18:48:07 +0545

    array for odd and even addition using array

--- Frontend/javascript/array/array.js
+++ Frontend/javascript/array/array.js
@@ -1,7 +1,30 @@
 //  let fruits = ["apple","pineapple","orange"]
 // console.log(fruits)
 
+  
+
 
 
 // let fruits = new Array("apple","pineapple","orange")
 // console.log(fruits[2])
+
+
+
+
+
+
+// let num1 = []
+// let num2 = []
+// for(i=0;i<=5;i++){
+// let userInput1 = Number(prompt("Enter a odd number: "))
+// let userInput2 = Number(prompt("Enter a even number: "))
+// num1[i] = userInput1
+// num2[i] = userInput2
+// console.log()
+// }
+
+
+//  for(k=0;k<=5;k++){
+// sum = num1[k] + num2[k]
+// console.log(`${num1[k]} + ${num2[k]} is ${sum}`)
+// }
