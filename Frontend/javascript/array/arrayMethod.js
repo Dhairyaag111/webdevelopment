52f54f7a5757d583b3fbf7fc127a358fa32b6b89
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-07-03 18:56:06 +0545

    method learning in array java script

---
+++ Frontend/javascript/array/arrayMethod.js
@@ -0,0 +1,110 @@
+// length method
+
+// let fruits = ["apple","mango","oragne","banana","watermelon"]
+// let size = fruits.length
+// console.log(size)
+ 
+ 
+// let array = []
+// console.log(array.length)
+
+// array.length = 5
+// console.log(array.length)
+
+
+
+// to string method
+
+// let num = [1,2,3,4,5]
+// let second = num.toString()
+// let sum = 0
+// for(i=1; i<num.length; i++){
+// sum = sum + num[i]
+// }
+// console.log(sum)
+
+
+
+// at method
+
+// let fruits = ["apple","mango","orange","banana","watermelon"]
+// let fruit = fruits.at(2)
+// console.log(fruit)
+
+
+
+// join method
+
+// let fruits = ["apple","mango","orange","banana","watermelon"]
+// let variable = fruits.join()
+// console.log(variable)
+// let variable2 = fruits.join(" ~ ")
+// console.log(variable2)
+
+
+
+// pop method
+
+// let fruits = ["apple","mango","orange","banana","watermelon"]
+// fruits.pop()
+// console.log(fruits)
+
+
+
+//  push method()
+ 
+// let fruits = ["apple","mango","orange","banana","watermelon"]
+// fruits.push("grapes")
+// console.log(fruits)
+
+
+
+// shift method
+
+// let fruits = ["apple","mango","orange","banana","watermelon"]
+// fruits.shift()
+// console.log(fruits)
+
+
+
+// let fruits = ["apple","mango","orange","banana","watermelon"]
+// let variable = fruits.shift()
+// console.log(variable)
+
+
+
+// unshift method
+
+// let fruits = ["apple","mango","orange","banana","watermelon"]
+// fruits.unshift("grapes")
+// console.log(fruits)
+
+
+
+
+
+
+
+//  let fruits = ["apple","mango","orange","banana","watermelon"]
+// fruits[5] = "grapes"
+//  console.log(fruits)
+
+
+
+
+
+
+//  let fruits = ["apple","mango","orange","banana","watermelon"]
+// fruits[fruits.length] = "grapes"
+//  console.log(fruits)
+
+
+
+// delete method
+
+//  let fruits = ["apple","mango","orange","banana","watermelon"]
+//  delete fruits[3]
+//  console.log(fruits)
+ 
+ 
+ 
