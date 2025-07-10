2504559f2037837506b426f06a41cadd1e5b765f
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-07-10 18:17:04 +0545

    for Each learning

---
+++ Frontend/javascript/array/Iteration.js
@@ -0,0 +1,32 @@
+// let fruits = ["apple","mango","orange","banana","watermelon"]
+// for(let i of fruits){
+//     console.log(i)
+// }
+
+
+// for each 
+
+    // let fruits = ["apple","mango","orange","banana","watermelon"]
+    // fruits.forEach(function (x){
+    //     console.log(x)
+    // })
+
+
+    // let fruits = ["apple","mango","orange","banana","watermelon"]
+// fruits.forEach(x =>{
+//     console.log(x)
+// })
+
+
+// let num = [2,4,6,8,10]
+// num.forEach(function (a){
+//     console.log(a*a)
+// }
+//     )
+
+
+// let fruits = ["apple","mango","orange","banana","watermelon"]
+// fruits.forEach(function(a,b){
+//     console.log("The index is "+a)
+//     console.log("The fruits is "+b)
+// })
