bbd61391fb8d292e4192d909567c037b946c7759
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-07-15 19:07:54 +0545

    learning methods like lenght,charAt,at,concat,slice,toUpperCase  and toLowerCase,trim,padStart and padEnd

---
+++ Frontend/javascript/string/stringMethods.js
@@ -0,0 +1,68 @@
+// .length
+
+// let name = "My name is Dhairya"
+// console.log(name.length)
+
+
+// .cahrAt
+
+// let name = "My name is Dhairya"
+// console.log(name.charAt(5))
+
+
+// .at
+
+// let name = "My name is Dhairya"
+// console.log(name.at(5))
+
+
+
+// let name = "My name is Dhairya"
+// console.log(name[5])
+
+
+// .concat
+
+// let name = "My name is Dhairya."
+// let data = "I live in Birtamode."
+// console.log(name.concat(" ",data))
+
+
+// .slice
+
+// let fruits = 'apple,banana,kiwi'
+// let slice = fruits.slice(6,12)
+// console.log(slice)
+
+//  let fruits = 'apple,banana,kiwi'
+//  let slice = fruits.slice(6,17)
+//  console.log(slice)
+
+//  let fruits = 'apple,banana,kiwi'
+//  let slice = fruits.slice(13)
+//  console.log(slice)
+
+//  let fruits = 'apple,banana,kiwi'
+//  let slice = fruits.slice(-11)
+//  console.log(slice)
+ 
+//   let fruits = 'apple,banana,kiwi'
+//  let slice = fruits.slice(-11,-5)
+//  console.log(slice)
+
+
+
+// .toUpperCase and toLowerCase
+
+// let firstName = "dhairya"
+// let secondName = "AGRAWAL"
+// console.log(firstName.toUpperCase(2))
+// console.log(secondName.toLowerCase(2))
+
+
+
+// .trim
+
+// let name = "    Dhairya    "
+// console.log(name)
+// console.log(name.trim())
