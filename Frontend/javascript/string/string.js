a681412a124ada390c6db6007477afb9a0672a8e
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-07-15 18:36:12 +0545

    some methods lite \n  \t and using == and === learning

--- Frontend/javascript/string/string.js
+++ Frontend/javascript/string/string.js
@@ -20,3 +20,22 @@
 
 // let name = 'It\'s me \'Dhairya\''
 // console.log(name)
+
+
+// console.log("The first line\n the second line")        
+// console.log("My name is dhairya \t using 't' 4 gaps ")   
+
+
+// let num1 = 4
+// let num2 = 4
+// console.log(num1==num2)
+
+
+// let num1 = 4
+// let num2 = "4"
+// console.log(num1==num2)
+
+
+// let num1 = 4
+// let num2 = "4"
+// console.log(num1===num2)
