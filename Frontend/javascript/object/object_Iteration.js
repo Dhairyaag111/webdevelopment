a7ee87dfd087e11307dedeef4ea02a9946193d3a
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-07-21 19:51:36 +0545

    to count the no. of properties of the object

--- Frontend/javascript/object/object_Iteration.js
+++ Frontend/javascript/object/object_Iteration.js
@@ -15,10 +15,26 @@
 //     age: 15,
 //     address: "birtamode",
 //     school: "Green Field World School",
-//     grade : "A",
+//     grade : "10 'A'"",
 //     house: "Yellow"
 // }
 
 // for (key in student){
 //     console.log(`${key} : ${student[key]}`)
 // }
+
+
+let student = {
+    name: "Dhairya",
+    age: 15,
+    address: "birtamode",
+    school: "Green Field World School",
+    grade : "10 'A'",
+    house: "Yellow",
+}
+let counter = 0
+for (key in student){
+        counter++
+}
+console.log(`Number of properties is ${counter}`)
+
