5daf40b3bf85dd5b497ec18bda156c374fa14c5e
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-07-21 18:55:32 +0545

    made an object with properties and printed using for and key

---
+++ Frontend/javascript/object/object_Iteration.js
@@ -0,0 +1,24 @@
+// let person = {
+//     name: "Dhairya",
+//     age: 15,
+//     isMarried: "No",
+//     address: "Birtamode,Jhapa"
+// }
+// for( let key in person){
+//     console.log(`${key} : ${person[key]}`)
+// }
+
+
+
+// let student = {
+//     name: "Dhairya",
+//     age: 15,
+//     address: "birtamode",
+//     school: "Green Field World School",
+//     grade : "A",
+//     house: "Yellow"
+// }
+
+// for (key in student){
+//     console.log(`${key} : ${student[key]}`)
+// }
