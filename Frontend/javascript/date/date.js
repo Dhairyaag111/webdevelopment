dc244195f450284408ecc2a2aed1596e26260181
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-07-25 19:16:35 +0545

    new course learning inside java script (i.e date) almost every method of date done

---
+++ Frontend/javascript/date/date.js
@@ -0,0 +1,56 @@
+// let date1 = new Date()
+// console.log(date1)
+
+
+// let date2 = Date()
+// console.log(date2)
+
+
+// let date3 = new Date("October 23, 2015 11:30:20 ")
+// console.log(date3)
+
+
+// let date4 = new Date("2025-07-25")
+// console.log(date4)
+
+
+// let date5 = new Date(2025,07,25,18,34,00)
+// console.log(date5)
+
+
+// let date6 = new Date(2025,07,35,18,34,00)
+// console.log(date6)
+
+
+// let date7 = new Date(25,07,35,18,34,00)
+// console.log(date7)
+
+
+// let date8= new Date()
+// console.log(date8.getFullYear())
+
+
+// let date9= new Date()
+// console.log(date9.getMonth())
+
+
+// let date10= new Date()
+// console.log(date10.getDate())
+
+
+// let date11= new Date()
+// console.log(date11.getDay())
+
+// let date12= new Date()
+// console.log(date12.getHours())
+
+// let date13= new Date()
+// console.log(date13.getMinutes())
+
+// let date14= new Date()
+// console.log(date14.getSeconds())
+
+// let date15 = new Date()
+// date15.setFullYear(2025)
+// date15.setMonth(11)
+// console.log(date15)
