c53df9b23f9b9cd661b0e4cdcb9e35f673a9bc5d
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-07-25 19:17:38 +0545

    new course learning in java script (i.e math floor )some methode of this topic is done

---
+++ Frontend/javascript/mathFloor/mathFloor.js
@@ -0,0 +1,24 @@
+// console.log( Math.PI)
+
+
+// console.log(Math.E)
+
+
+// let num1 =  10
+// let num2 = 33.378
+// console.log(Math.round(num2))
+
+
+// let num1 =  10
+// let num2 = 33.378
+// console.log(Math.floor(num2))
+
+
+// let num1 =  10
+// let num2 = 33.045
+// console.log(Math.ceil(num2))
+
+
+// let num1 =  10
+// let num2 = 33.378
+// console.log(Math.trunc(num2)) 
