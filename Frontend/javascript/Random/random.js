8d7741f1667be741d67fe347ed2b8f03977a05ff
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-07-28 18:30:55 +0545

    new method (i.e round) learned

---
+++ Frontend/javascript/Random/random.js
@@ -0,0 +1,15 @@
+// console.log(Math.random())
+
+
+// console.log(Math.random()*10)
+
+
+// console.log(Math.floor(Math.random()*10))
+
+
+// console.log(Math.floor(Math.random()*11))
+
+
+// console.log(Math.floor(Math.random()*100))
+
+
