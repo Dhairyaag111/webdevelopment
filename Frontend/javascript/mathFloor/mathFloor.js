7b78a33d3d2ea55fc04fa740c60055b903168328
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-07-28 18:18:31 +0545

    Math method in js completed

--- Frontend/javascript/mathFloor/mathFloor.js
+++ Frontend/javascript/mathFloor/mathFloor.js
@@ -22,3 +22,21 @@
 // let num1 =  10
 // let num2 = 33.378
 // console.log(Math.trunc(num2)) 
+
+
+// let num = -2
+// console.log(Math.abs(num))
+
+
+// console.log(Math.pow(2,4))
+
+
+// console.log(Math.sqrt(49))
+
+
+// console.log(Math.min(12,43,634,124,65,1,64,77))
+
+
+// console.log(Math.max(12,43,634,124,65,1,64,77))
+
+
