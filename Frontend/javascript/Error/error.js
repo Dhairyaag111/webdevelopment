5ed340399eed6f0d667743702941076cbf6b3e5b
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-07-29 18:28:05 +0545

    theory of error handling completed

---
+++ Frontend/javascript/Error/error.js
@@ -0,0 +1,23 @@
+//  handling real world errors in java script or any other programming languages is known as error handling.
+
+// In java script we use try, catch, finally, throw to handle all types of error
+
+// Example:- We are building a payment system , user enters a card number. If the number is invalid our app might crash so, at that time we will catch errors, show a message, and continue running the app.
+
+// So, this is what error handling is made for.
+
+// Try - This is the bloch where we try to run some code
+
+// Catch - This is the block where errors are cut
+
+// Finally - This is the optinal block that runs whatever happens
+
+// Throw - This is the block where we create our manual errors and throw it
+
+
+
+
+
+
+
+
