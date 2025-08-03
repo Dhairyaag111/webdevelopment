59e0b67880194d779c08876de0f58ae90038bf91
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-08-03 18:39:17 +0545

    class learning in js today

---
+++ Frontend/javascript/class/class.js
@@ -0,0 +1,21 @@
+// class car{
+//     constructor(brand,color,power,capacity,price){
+//     this.brand = brand
+//     this.color = color
+//     this.power = power
+//     this.capacity = capacity
+//     this.price = price
+//     }
+//     properties(){
+//         console.log(`The car brand is ${this.brand} nad it's color is ${this.color}. This car gives power of ${this.power} also ${this.capacity} can sit in this car.The price of car is Rs.${this.price}`)
+//     }
+// }
+    
+//     let obj1 = new car("BMW","White","30000 hp",5,10000000)
+//     obj1.properties()
+//     console.log("")
+//    let obj2 = new car("MUSTANG","Red","123456 hp",3,1450000)
+//     obj2.properties()
+//     console.log("")
+//     let obj3 = new car("AUDI","Blue","98765 hp",6,500000)
+//     obj3.properties()
