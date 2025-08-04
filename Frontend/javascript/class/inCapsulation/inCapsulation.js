8d18ed88ad99b9f2a37800d409b90767ee1a35f7
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-08-04 18:42:26 +0545

    new class method get set learning

---
+++ Frontend/javascript/class/inCapsulation/inCapsulation.js
@@ -0,0 +1,11 @@
+// class person{
+//         constructor(name,age){
+//             this.name = name
+//             this.age = age
+//         }
+//         get age1(){
+//             return this.age
+//         }
+// }
+// let object = new person("Dhairya",15)
+// console.log(object.age1)
