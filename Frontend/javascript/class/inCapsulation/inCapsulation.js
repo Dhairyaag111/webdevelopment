ddc684c56bd59add2e99e542ba0f873b137c8822
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-08-05 18:30:04 +0545

    setter and getter combined learning in class (js)

--- Frontend/javascript/class/inCapsulation/inCapsulation.js
+++ Frontend/javascript/class/inCapsulation/inCapsulation.js
@@ -9,3 +9,35 @@
 // }
 // let object = new person("Dhairya",15)
 // console.log(object.age1)
+// console.log(object.age)
+
+
+
+
+
+
+
+//     class person{
+//     constructor(name,age){
+//         this.name = name
+//         this.age = age
+//     }
+//     get age1(){
+//         return this.age
+//     }
+    
+//     set age1(value){
+//         if(value>0){
+//             this.age = value
+//         }
+//         else{
+//             console.log("Age is less than 0")
+//         }
+//     }
+// }
+
+// let obj = new person("Dhairya",15)
+// console.log(obj.age1)
+
+// obj.age1 = 20
+// console.log(obj.age1)
