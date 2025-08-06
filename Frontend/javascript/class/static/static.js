4ed60220b8457aa59938453459da3e95d69f0cda
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-08-06 19:16:51 +0545

    more tasks done using static in js

--- Frontend/javascript/class/static/static.js
+++ Frontend/javascript/class/static/static.js
@@ -20,3 +20,36 @@
 // console.log(Calculate.value)
 
 
+
+
+
+// class Counter{
+//     static value = 0
+//     constructor(name){
+//         this.name = name
+//         Counter.value ++
+//     }
+    
+//     static getTotal(){
+//         console.log("The number of object is: "+Counter.value)
+//     }
+// }
+
+// let obj1 = new Counter("shyam")
+// let obj2 = new Counter("ram")
+// Counter.getTotal()
+
+
+
+
+
+// 1.Static method to validate phone number length
+// Rule: If phone number length is not 10, return "Invalid"
+
+// 2. Static login checker without creating user
+// Task:
+// Create a class with a static method that takes username and password and returns success/fail.
+
+// 3. Static method to capitalize the first letter of a string
+
+// 4. Generate Random 6-digit OTP using static
