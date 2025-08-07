225f5016ed7ec183aeda0173a0846ac1bfe3e478
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-08-07 18:25:01 +0545

    all tasks done

--- Frontend/javascript/class/static/static.js
+++ Frontend/javascript/class/static/static.js
@@ -46,10 +46,70 @@
 // 1.Static method to validate phone number length
 // Rule: If phone number length is not 10, return "Invalid"
 
+
+
+// class phoneNumber{
+//     static getNumber(number){
+//         if(number.length === 10){ 
+//             return "Valid phone number"
+//         }
+//         else{
+//         return "Invalid Phone number."
+//         }
+//     }
+// }
+
+// console.log(phoneNumber.getNumber("1236142124"))
+// console.log(phoneNumber.getNumber("123614212"))
+
+
+
+
+
+
 // 2. Static login checker without creating user
 // Task:
 // Create a class with a static method that takes username and password and returns success/fail.
 
+
+// class login{
+//     static checker(username,pass){
+//         if(username==="admin" && pass===1234){
+//             return "Login Successfull"
+//         }
+//         else{
+//             return "Login failed."
+//         }
+//     }
+// }
+
+// console.log(login.checker("Dhairya","qwertyuiop"))
+// console.log(login.checker("admin",1234))
+
+
+
+
 // 3. Static method to capitalize the first letter of a string
 
+// class Upper{
+//     static upperCase(str){
+//         return str[0].toUpperCase() + str.slice(1)
+//     }
+// }
+// console.log(Upper.upperCase("nayan"))
+
+
+
+
+
 // 4. Generate Random 6-digit OTP using static
+
+
+
+// class random{
+//     static otp(){
+//     return  (Math.floor(Math.random()*1000000))
+//     }
+// }
+
+// console.log(random.otp())
