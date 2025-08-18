8c8b2db9ba2bfc757dac9689632c7485b7683741
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-08-18 12:07:57 +0545

    among 3 tow donw 1 error

---
+++ Frontend/javascript/class/inHeritance/inHeritance.js
@@ -0,0 +1,93 @@
+//  create a class animal with a method sound, create a class dog that inherits parent class and over writes parent method sound
+
+
+// class Animal{
+//     sound(){
+//         console.log("All animals hears")
+//     }
+// }
+// class dog extends Animal{
+//     sound(){
+//         console.log("Dog hears")
+//     }
+// }
+
+// let dogObj = new dog()
+// dogObj.sound()
+
+
+
+
+
+//  show multi level inheritance
+
+
+
+// class animal{
+//     walk(){
+//         console.log("All animals walks")
+//     }
+// }
+// class lion extends animal{
+//     eat(){
+//         console.log("It is a carnivorous")
+//     }
+// }
+
+// class tiger extends lion{
+//     run(){
+//         console.log("This animal runs fast")
+//     }
+// }
+// class cheetah extends tiger{
+//     height(){
+//         console.log("It's height is about 32 inch")
+//     }
+// }
+
+// let obj = new lion()
+// obj.walk()
+// let obj1 = new tiger()
+// obj1.eat()
+// let obj2 = new cheetah()
+// obj2.run()
+// let obj3 = new cheetah()
+// obj3.height()
+
+
+
+
+
+
+//  show hierarchial inheritance(create a parent class shape , create 3 child class    circle =, square, rectangle and calculate thieir area)
+
+
+let radius,length,breadth=0
+class shape{
+    property(){   
+     radius = 7
+     length = 10
+     breadth = 5
+}
+}
+
+class circle extends shape{
+    area(){
+        console.log("Area of circle is "+3.1415*radius*radius)
+    }
+}
+
+class square extends shape{
+    area(){
+        console.log("Area of square is "+length*length)
+    }
+}
+
+class rectangle extends shape{
+    area(){
+        console.log("Area of rectangle is "+length*breadth)
+    }
+}
+
+let obj = new circle()
+obj.area()
