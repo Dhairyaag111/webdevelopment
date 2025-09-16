9f782d2716244c6b466f197f12609967b7e0069f
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-09-16 18:28:14 +0545

    a short breif writter adout synchronous and asynchronous also done a basic example

---
+++ Frontend/javascript/Asynchronous/asynchronous.js
@@ -0,0 +1,21 @@
+// Syncronous is the default in java script. 
+// Our code runs line by line one after the other. 
+// If one line is taking time rest waits.
+// So, this is the syncronous code behavior is java script.
+
+
+// In asynchronous way our code dose not waits for slower tasks like API calls, timers, file reading.
+// It transfers those tasks and continue with the rest of the code.
+// Once the task is done it notifies back using call backs, promises or Async/Await.
+
+
+
+
+
+
+// console.log("Print1")
+// setTimeout(()=>{
+//     console.log("Printing after 4 seconds.Print2")
+// },4000)
+
+// console.log("Print3")
