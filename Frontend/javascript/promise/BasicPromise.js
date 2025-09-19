657ce42271c0a754330a961a92f28e1f55fed642
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-09-19 17:03:34 +0545

    learned promise basics

---
+++ Frontend/javascript/promise/BasicPromise.js
@@ -0,0 +1,15 @@
+// let userInput = Number(prompt("Enter your age: "))
+// let promise = new Promise((resolve, reject) => {
+//     if (userInput > 17) {
+//         resolve("Eligible for vote")
+//     } 
+//     else {
+//         reject(" Not eligible for vote")
+//     }
+// })
+
+// promise.then(function(message) {
+//     console.log(message)
+// }).catch(function(err) {
+//     console.log(err)
+// })
