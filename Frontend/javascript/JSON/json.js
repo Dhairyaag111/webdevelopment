cc5c7b06a6e713f3e7382aa9ff5b340f26e019bf
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-10-08 18:32:36 +0545

    used some JSON methods like parse and stringify

--- Frontend/javascript/JSON/json.js
+++ Frontend/javascript/JSON/json.js
@@ -13,3 +13,21 @@
 //     }
 // }
 //     console.log(json)
+
+
+
+
+// when we have to change object into string we have to use "JSON.stingify(object)"
+// when we have to change string into object we have to use "JSON.parse(string or string variable)"
+
+// example::
+
+// let data1 = '{"fruits":"apple","vegetable":"potato"}'
+// console.log(data1)
+// console.log(typeof(data1))
+// let data2 = JSON.parse(data1)
+// console.log(data2)
+// console.log(typeof(data2))
+// let data3 = JSON.stringify(data2)
+// console.log(data3)
+// console.log(typeof(data3))
