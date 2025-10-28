a3e93cfe9c0b297ce22106f9cdd6102575524dd9
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-10-28 18:33:01 +0545

    learned ajax

--- Frontend/javascript/Ajax/ajax.js
+++ Frontend/javascript/Ajax/ajax.js
@@ -1,3 +1,14 @@
 // Asynchronous Javascript And Xml (full form).
 // Its is used to update data without refreshing.
 
+
+
+
+fetch("https://6900b732ff8792314bb1b8a.mockapi.io/test")
+.then(a=>{
+    return a.json()
+}).then(b=>{
+    console.log(b)
+}).catch(c=>{
+    console.log("some api keys are missing")
+})
