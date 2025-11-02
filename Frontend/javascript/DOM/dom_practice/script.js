7e3c1fae669fa54592188cfecb17b3c5413f1dff
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-11-02 19:17:24 +0545

    learned some things of DOM in js

---
+++ Frontend/javascript/DOM/dom_practice/script.js
@@ -0,0 +1,36 @@
+// Get Element By Id:-
+// It only access the first matched element. It give the output in single form.
+
+// let element = document.getElementById('first')
+// console.log(element)
+
+// let element = document.getElementById('first')
+// console.log(element.textContent)
+
+
+
+// Get Element by Class name:-
+// It can access every elements matche by class. It give the output in array form.
+
+// let element = document.getElementsByClassName('second')
+// console.log(element)
+
+// let element = document.getElementsByClassName('second')
+// console.log(element[0].textContent)
+// console.log(element[1].textContent)
+
+
+
+// Get Element By Tag:-
+// It can access every elements matched by tag. It give the output in array form.
+
+// let element = document.getElementsByTagName('h1')
+// console.log(element)
+
+// let element = document.getElementsByTagName('h1')
+// console.log(element[0].textContent)
+// console.log(element[1].textContent)
+// console.log(element[2].textContent)
+
+
+
