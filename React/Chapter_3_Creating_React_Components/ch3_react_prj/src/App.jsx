a769fd5fadb18894c2ede8eb83a84c62b71c745b
dhairyaag111 <dhairyaagrawal111@gmail.com>
2025-12-31 18:15:51 +0545

    learned how to create vairable and use

--- React/Chapter_3_Creating_React_Components/ch3_react_prj/src/App.jsx
+++ React/Chapter_3_Creating_React_Components/ch3_react_prj/src/App.jsx
@@ -1,15 +1,30 @@
-import FirstButton from "./button_comp"
-import { SecondButton } from "./button_comp"
-import { ThirdButton } from "./button_comp"
-function App() {
+// import FirstButton from "./button_comp"
+// import { SecondButton } from "./button_comp"
+// import { ThirdButton } from "./button_comp"
+// function App() {
+//   return (
+//     <div>
+//       <FirstButton></FirstButton>
+//       <SecondButton></SecondButton>
+//       <ThirdButton></ThirdButton>
+//     </div>
+//   )
+
+// }
+// export default App
+
+
+
+
+
+
+
+import { Hello } from "./HelloDynamic"
+function App(){
   return (
     <div>
-      <FirstButton></FirstButton>
-      <SecondButton></SecondButton>
-      <ThirdButton></ThirdButton>
-dddd
+    <Hello></Hello>
     </div>
   )
-
 }
 export default App
---
+++ React/Chapter_3_Creating_React_Components/ch3_react_prj/src/HelloDynamic.jsx
@@ -0,0 +1,6 @@
+export function Hello() {
+    let name = 'Dhairya Agrawal'
+    let address = "Birtamode"
+    let age = "16"
+        return <h1>Hello I am {name}. I am from {address}. I am {age} yeras old.</h1>
+}
