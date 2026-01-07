5eb43fadaf997be87ddfe35f8b8cc74af35e7431
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-01-07 18:30:38 +0545

    learned three operator (if-else, turnery, logical)

---
+++ React/Chapter_4_Fragment_Map/ch4_conditional_rendering/src/App.jsx
@@ -0,0 +1,25 @@
+export default function App() {
+    let arr = ["apple", "mango", "orange", "pineapple", "guava"]
+    // arr = []
+
+    // if (arr.length === 0) {
+    //     return <>
+    //         <h3>Sorry no fruits</h3>
+    //     </>
+    // }
+
+    // let variable = (arr.length === 0) ? <h3>Sorry no fruits</h3> : null
+
+    // let variable = ((arr.length===0) && <><h3>Sorry no fruits</h3></>)
+    return (
+        <>
+            {/* {variable} */}
+            <ul class="list-group">
+                {arr.map(fruits => (
+                    <li key={fruits} className="list-group-item">{fruits}</li>
+                ))}
+            </ul>
+        </>
+    )
+
+}
