ea37a06a52beb314b1b5342fc9bd987a5c4cd0cf
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-01-18 19:03:03 +0545

    learned about CRUD (database) but not updated in vs code

--- React/Chapter_5_CSS_Module/ch5_basic/src/components/Hobbies.jsx
+++ React/Chapter_5_CSS_Module/ch5_basic/src/components/Hobbies.jsx
@@ -1,6 +1,5 @@
 function Hobbies(props) {
     return <>
-    <div></div>
         <ol type="a">
             <li className="list one">{props.SentData.hobbies[0]}</li>
             <li className="list two">{props.SentData.hobbies[1]}</li>
