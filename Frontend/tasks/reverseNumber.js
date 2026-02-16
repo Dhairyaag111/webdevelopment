97072a90b21fc830bf0c4d3b7cdc4a2bb976184c
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-02-16 18:22:30 +0545

    tried to make multi digit reverse program using js

---
+++ Frontend/tasks/reverseNumber.js
@@ -0,0 +1,11 @@
+// let num = Number(prompt("Enter any number: "))
+// let len = num.toString().length
+// let reverse = 0
+
+// // for(i=1;i<=len;i++){
+// while(num>0){
+//     let num2 = num%10
+//     reverse = reverse*10 + num2
+//     num = num/10>>0
+// }
+// console.log(reverse)
