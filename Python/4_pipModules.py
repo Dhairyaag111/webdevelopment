784cb3de02fc1163cfa97fc56ac3700bdae29e36
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-02-19 19:16:25 +0545

    learned about pip modules in python

---
+++ Python/4_pipModules.py
@@ -0,0 +1,14 @@
+# Modules -  It is the method of using code developed by other developer. Modules are well tested, less error, used by   many peoples. Modules are like code library which can be use to borrow a code written by somebody else.
+
+
+# There are 2 types of modules in python:-
+
+# i.  Built in modules:- This modules are attached in python, we can access it after downloading and installing python.
+# ii. External modules:-This are the type of modules whick are not attached in python, built by other developers, we need to use pip command to access those modules.
+
+# PIP Command:- PIP is a package manager which stands for prefered installer program. This command is used to install various python modules.
+# Examples:-
+
+# pip install pandas:- This will install a module named pandas. However it is the wrong and dangerous way to use pip command.
+
+# The best way to use pip command is :- python -m pip install pandas.  
