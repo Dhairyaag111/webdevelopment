c8a086fb1b08992fa75c5252a4f9ba2272f410ee
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-02-19 19:24:26 +0545

    leared about escape sequence

---
+++ Python/5_escapeSequence.py
@@ -0,0 +1,7 @@
+print("My name is Dhairya Agrawal \nI am 16 years old.")
+# \n :- This will force to go to a new line.
+
+
+print(f"My name is \"Dhairya\"")
+# \"\" :- Any thing inside this goes into double quotation.
+# \'\' :- Any thing inside this goes into single quotation.
