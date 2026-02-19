6ebdab4ff3f770aaad074f43bedf425fbf84422a
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-02-19 19:31:29 +0545

    learned about seperator in pythin

---
+++ Python/6_seperator.py
@@ -0,0 +1,9 @@
+# Seperator is uesd to seperate multi line values inside the print statement.
+# example:-
+print("apple", "mamgo", "banana", "kiwi", "orange", sep="<>")
+
+# Ending is used to append something to the end of the print statement.
+# example:-
+print("apple", "mango", "banana", "kiwi" , "orange", end="pineapple")
+
+print("apple", "mango", "banana", "kiwi" , "orange", end="\npineapple")
