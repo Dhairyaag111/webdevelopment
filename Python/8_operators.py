cabcd98670686dc5fe7c373fb685df0aeea7dd52
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-02-20 18:46:04 +0545

    operators and userInput learned in python

---
+++ Python/8_operators.py
@@ -0,0 +1,17 @@
+a = 15
+b = 4
+
+
+print(a+b)
+
+print(a-b)
+
+print(a*b)
+
+print(a/b)
+
+print(a//b)
+
+print(a%b)
+
+print(a**b)
---
+++ Python/9_userInput.py
@@ -0,0 +1,15 @@
+# We use input() in python to take user input in python.
+
+a = int(input("Enter any number "))
+b = int(input("Enter another number "))
+
+print(a+b)
+
+
+a1 = int(input("Enter length of first side on triangle "))
+a2 = int(input("Enter length of second side on triangle "))
+a3 = int(input("Enter length of third side on triangle "))
+
+perimeter = a1+a2+a3
+
+print(f"The perimeter of triangle is {perimeter}.")
