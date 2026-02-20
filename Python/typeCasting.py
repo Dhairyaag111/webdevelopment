e38fcb27e7e0573123b4eb982c94ddc4d6d2c57d
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-02-20 18:33:52 +0545

    learned about type casting in python

---
+++ Python/typeCasting.py
@@ -0,0 +1,20 @@
+# Typecasting is the process of converting a variable from one datatype to another datatype.
+
+name = "Dhairya"
+age = 16
+gpa = 3.50
+isMarried = False
+
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(isMarried))
+
+# Datatype Converting
+
+print(bool(name))
+print(str(age))
+print(int(gpa))
+print(str(isMarried))
+
