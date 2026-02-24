14a97f2209fea104f6d9719624a2edd9c2847176
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-02-24 19:24:22 +0545

    learned if elif and else with some task

---
+++ Python/else_if.py
@@ -0,0 +1,20 @@
+# age = int(input("Enter your age: "))
+
+# if(age>=18):
+#     print("You are eligible for vote")
+
+# elif(age>=0 & age<18):
+#     print("You are not eligible for vote")
+    
+# else:
+#     print("Invalid input")
+
+
+
+
+name = input("Enter your name: ")
+
+if(len(name) == 0):
+    print("Enter your name, it should not be empty")
+else:
+    print(f"Your name is {name}.")
--- Python/exercise.py
+++ Python/exercise.py
@@ -1,20 +1,27 @@
-input1 = int(input("Enter the first number: "))
-input2 = int(input("Enter the second number: "))
+# input1 = int(input("Enter the first number: "))
+# input2 = int(input("Enter the second number: "))
 
-print(f"The sum of {input1} and {input2} is {input1+input2}.")
+# print(f"The sum of {input1} and {input2} is {input1+input2}.")
 
-print(f"The difference of {input1} and {input2} is {input1-input2}.")
+# print(f"The difference of {input1} and {input2} is {input1-input2}.")
  
-print(f"The product of {input1} and {input2} is {input1*input2}.")
+# print(f"The product of {input1} and {input2} is {input1*input2}.")
 
-print(f"The division of {input1} and {input2} is {input1/input2}.")
-
-
-
-import math
+# print(f"The division of {input1} and {input2} is {input1/input2}.")
 
 
-perpendicular = int(input("Enter perpendicular: "))
-base = int(input("Enter base: "))
-hypotenuse = math.sqrt(pow(perpendicular,2) + pow(base,2))
-print(hypotenuse)
+
+
+
+# import math
+
+
+# perpendicular = int(input("Enter perpendicular: "))
+# base = int(input("Enter base: "))
+# hypotenuse = math.sqrt(pow(perpendicular,2) + pow(base,2))
+# print(hypotenuse)
+
+
+
+
+
