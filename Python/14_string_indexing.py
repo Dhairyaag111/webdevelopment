9a826b9152754c89e295e61c48995364c96046f3
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-02-24 18:54:05 +0545

    string indexing learned in python

---
+++ Python/14_string_indexing.py
@@ -0,0 +1,18 @@
+fruit = "Watermelon"
+
+print(fruit[1])
+
+print(fruit[0:4])
+print(fruit[3:7])
+
+print(fruit[1:9:2])
+
+print(fruit[:9])
+
+print(fruit[::2])
+
+print(fruit[-3:-1])
+
+print(fruit[-9:-1:2])
+
+print(fruit[::-1]) # It will make reverse
