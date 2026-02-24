1cb442673e3e585dab0f7372221c542f0339eb6b
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-02-24 18:34:58 +0545

    learned about some functions while print ing process

---
+++ Python/13_stirng_method.py
@@ -0,0 +1,26 @@
+name = "Dhairya Agrawal"
+digit_str = "12"
+alpha = "qwerhrhrbrb"
+
+print(len(name))
+
+print(name.find("a"))
+
+print(name.rfind("a"))
+
+print(name.capitalize())
+
+print(name.upper())
+
+print(name.lower())
+
+print(name.isdigit())
+print(digit_str.isdigit())
+
+print(name.isalpha())
+print(alpha.isalpha()) # only alphabet no space no special character
+
+print(name.count("a"))
+
+print(name.replace("a","b"))
+
