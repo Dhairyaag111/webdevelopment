1559eaefe45cb0e174f94fd93003b67cb589a2a8
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-02-25 19:27:01 +0545

    learned more about for loop with jumping and skipping task

---
+++ Python/18_for_loop.py
@@ -0,0 +1,19 @@
+for i in range (10):
+    print(f"5 X {i+1} = {5*(i+1)}")
+    
+    
+    
+for i in range(100,501,100):
+    print(i)
+    
+
+
+for i in reversed(range(100,501,100)):
+    print(i)
+    
+    
+    
+for i in range(10):
+    if(i==6):
+        continue
+    print(i)
