a51910cc7917bb0a5e487c3e66652ef96e5596cc
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-02-27 19:16:03 +0545

    deleted previous task

--- Python/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-num = (input("Enter a number: "))
-# while(type(num)!=int):
-#     print("The input should valid.")
-#     num = (input("Enter a number: "))
-# if(num%2==0):
-#     print(f"{num} is an even number.")
-# elif(num%2==1):    
-#     print(f"{num} is an odd number.")
