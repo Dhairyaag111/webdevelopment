d85875cf867c1fc7f387b2c94e8dad1bbebccd29
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-02-27 19:19:39 +0545

    positive negative checker but throw error if num is 0

--- Python/19_while_loop.py
+++ Python/19_while_loop.py
@@ -9,3 +9,11 @@
 
 
 
+# num = int(input("Enter a number: "))
+# while(num==0):
+#     print("The number should not be 0")
+#     num = int(input("Enter a number: "))
+# if(num>0):
+#     print(f"{num} is a positive number.")
+# elif(num<0):
+#     print(f"{num} is a negative number.")
