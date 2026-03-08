1ce1d10b72a6d5c19fcefdadf9010fd4bb81a0db
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-03-08 18:42:14 +0545

    learned about tuples in python with its methods

---
+++ Python/22_tuples.py
@@ -0,0 +1,56 @@
+# Tuples are used to store multiple values inside a small brackets.
+# It is ordered and in-mutable.
+# Duplicate data is allowed in tuples.
+# It is not addable.
+
+
+
+
+# tuple1= ("Hello","World","Hello","Everyone",1011,None,True,False)
+# print(tuple1)
+# print(type(tuple1))
+
+
+
+
+# tuple2 = ()  #Empty tuple
+# print(tuple2)
+# print(type(tuple2))
+
+
+
+
+# tuple3 = ("Dhairya")  #Minimun 2 values are needed
+# print(type(tuple3))
+
+
+
+
+
+
+# Mthods in tuple
+
+
+fruits_tuple = ("Apple","Mango","Orange","Kiwi","Watermelon")
+
+
+# print(len(fruits_tuple))
+
+
+# print("Apple" in fruits_tuple)
+
+
+# print(fruits_tuple.count("Watermelon"))
+
+
+
+
+# process to add something in tuple
+
+# fruit_list = list(fruits_tuple)
+# fruit_list.append("Jackfruit")
+# fruits_tuple = tuple(fruit_list)
+# print(fruits_tuple)
+
+
+
