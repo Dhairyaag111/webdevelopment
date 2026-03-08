f9595fd5b79ec6def19f3beeca6c6668f05c76bf
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-03-08 19:11:00 +0545

    learned about dictionary methods in pythond

--- Python/24_dictionary.py
+++ Python/24_dictionary.py
@@ -28,18 +28,61 @@ dict1 = {
 
 
 
-print(dict1.keys())
-print(dict1.values())
+
+# print(dict1.keys())
+# print(dict1.values())
 
 
 
-for key in dict1.values():
-    print(key)
+
+# for key in dict1.values():
+#     print(key)
 
 
-for val in dict1.values():
-    print(val)
+# for val in dict1.values():
+#     print(val)
     
 
-for key,value in dict1.items():
-    print(f"{key} : {value}")
+# for key,value in dict1.items():
+#     print(f"{key} : {value}")
+    
+
+
+
+# dict2 = {
+#     "address" : "Birtamode",
+#     "country" : "Nepal"
+# }
+# dict2.update(dict2)
+# print(dict1)
+
+
+
+
+# emp_dict = dict()  # Empty Dictionary
+# print(type(emp_dict))
+
+
+
+
+# print(dict1.clear())
+
+
+
+
+# del dict1   # Deletes the dictionary
+# print(dict1)
+
+
+
+
+# dict1.pop("name")
+# print(dict1)
+
+
+
+# dict1.popitem()
+# print(dict1)
+
+
+
