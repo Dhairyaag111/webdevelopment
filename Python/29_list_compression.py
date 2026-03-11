0affb4fc6add9d8daa9f700f63d58d21f7187bd4
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-03-11 19:10:24 +0545

    learned list compression with various task in python

---
+++ Python/29_list_compression.py
@@ -0,0 +1,78 @@
+import random
+
+
+
+
+# nums = [random.randint(100,2000),random.randint(100,2000),random.randint(100,2000),random.randint(100,2000),random.randint(100,2000),random.randint(100,2000),random.randint(100,2000),random.randint(100,2000),random.randint(100,2000),random.randint(100,2000)]
+# five_and_ten = [num for num in nums if num%5==0 and num%10==0]
+# nfive_and_nten = [num for num in nums if num%5!=0 and num%10!=0]
+# print(five_and_ten)
+# print(nfive_and_nten)
+
+
+
+
+
+
+# fruits = ["apple","orange","mango","kiwi","guava"]
+# length = [len(fruit) for fruit in fruits]
+# print(fruits)
+# print(length)
+
+
+
+
+
+
+# name = "PROGRAMMING"
+# vowels = [vow for vow in name if vow in ["A","E","I","O","U"]]
+# print(vowels)
+
+
+
+
+
+
+
+
+# nums = [1,-2,-3,5,4,-8]
+# pos = [num if num>=0 else 0 for num in nums]
+# print(pos)
+
+
+
+
+
+
+# fruits = ["apple","","banana","","orange"]
+# list1 = [fruit for fruit in fruits if len(fruit)!=0]
+# print(list1)
+
+
+
+
+
+
+
+
+# fruits = ["apple","banana","orange"]
+# list1 = [fruit[0] for fruit in fruits]
+# print(list1)
+
+
+
+
+
+
+
+# cel = [100,112,1,17]
+# fah = [(9/5)*a+32 for a in cel]
+# print(fah)
+
+
+
+
+
+
+
+
