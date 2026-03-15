38be49ebede79292de9f5f6a4ebd76e5c264efe1
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-03-15 18:34:08 +0545

    learned enumerate in python

---
+++ Python/32_enumerate.py
@@ -0,0 +1,38 @@
+# name = "Dhairya Agrawal"
+
+# for index, i in enumerate(name):
+#     print(index,i)
+    
+    
+    
+    
+# list1 = ["Apple","Mango","Orange","Pineapple"]
+# for index, i in enumerate(list1):
+#     print(index,i)
+    
+    
+    
+
+
+
+# tuple1 = ("Apple","Mango","Orange","Pineapple")
+# for index, i in enumerate(tuple1):
+#     print(index,i)
+    
+    
+    
+    
+    
+# set1 = {"Apple","Mango","Orange","Pineapple"}
+# for index, i in enumerate(set1):
+#     print(index,i)
+
+
+
+
+
+
+
+# fruits_Set = {"Apple","Mango","Orange","Pineapple"}
+# for index, i in enumerate(fruits_Set,start=100):
+#     print(index,i)
