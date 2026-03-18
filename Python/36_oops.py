8a241f9ff7da588d31b2508abd6f474815d5e25b
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-03-18 18:55:09 +0545

    learned theory of OOP in python

---
+++ Python/36_oops.py
@@ -0,0 +1,16 @@
+# There are two types of programming language : one is procudure oriented and the other one is object oriented.
+# POP(Procedural Oriented Programming) : It follows top to bottom approach, runs squentially, use functions mainly.
+# OOP(Object Oriented Programming) : It follows bottom up approach, uses real world entities called objects, class.
+
+
+# Class : It is a blueprint of real world. 
+# Objects : Instance(Part) of class, has its own method.
+
+
+# There are 4 pillars in OOP
+# i.   Encapsulation
+# ii.  Polymorphism
+# iii. Inheritance
+# iv.  Abstraction
+
+
