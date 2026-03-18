f14656c504e797b8b168da779196ce4c80f4743f
dhairyaag111 <dhairyaagrawal111@gmail.com>
2026-03-18 19:25:07 +0545

    learned about class and objects in OOp with programs

---
+++ Python/37_class_objects.py
@@ -0,0 +1,31 @@
+# class Dhairya:
+#     print("I am Dhairya Agrawal")
+#     name = "Dhairya"
+#     age = 16
+#     isMarried = False
+    
+#     def info(self):
+#         print(f"My name is {self.name}. My age is {self.age}. I am {"married" if self.isMarried  else "not married"}")
+    
+    
+    
+# obj1 = Dhairya()
+# print(obj1)
+# print(obj1.name)
+# print(obj1.age)
+# print(obj1.isMarried)
+# obj1.info()
+
+
+
+
+
+
+class area:
+    l = 10
+    b = 7
+    def  evaluate(self):
+        print(f"The area is {self.l*self.b}")
+
+obj2 = area()
+obj2.evaluate()
